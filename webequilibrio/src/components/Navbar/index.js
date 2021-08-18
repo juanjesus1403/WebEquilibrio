855af42a46b695de005ee01fc6db2a99a976b65e
Juan Jesus Enciso Quesquen <juanjesus_eq96@hotmail.com>
2021-08-17 20:52:17 -0500

    Navbar completo

---
+++ webequilibrio/src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {
+  Nav, 
+  NavbarContainer, 
+  NavLogo, 
+  MobileIcon, 
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to ='/'>Equilibrio</NavLogo>
+          <MobileIcon>
+              <FaBars/>
+          </MobileIcon>  
+          <NavMenu>
+            <NavItem>
+              <NavLinks to = 'Nosotros'> Nosotros </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to = 'Servicios'> Servicios </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to = 'Proyectos'> Proyectos </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to = 'Ingresa'> Ingresa </NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/Donar">Donar</NavBtnLink>
+          </NavBtn>         
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
+
---
+++ webequilibrio/src/views/HomeView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function HomeView() {
+  return (
+    <div>
+      <h1>WebEquilibrio</h1>
+     
+    </div>
+  )
+}
